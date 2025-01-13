26899a90acb7ea204a9f8391dd7190ca08c54b11
Azak201 <123773404+Azak201@users.noreply.github.com>
2025-01-13 10:42:09 +0300

    Update notes main.c

--- push_swap/srcs/main.c
+++ push_swap/srcs/main.c
@@ -12,6 +12,14 @@
 
 #include <push_swap.h>
 
+/*the main function work as follow , splitter functoin take the argc & argv
+and return 2D array that have all inputs as a strings .
+the handler function take the array of inputs and if there any problem with it
+the function will call exit and break the program otherwise it will back to main.
+after that the inserter function will take the array of inputs and convert it to 
+linked list then will send it to function that will sort it if ther any error 
+the inserter will return 1 that will make the condition of if(val != 0) true then
+it will free and exit the programm in case of sucsess the inserter will return 0. */
 int	main(int arc, char **arv)
 {
 	char	**inputs;
